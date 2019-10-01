0a3d6832df459e57c7e597413045f01ada35a531
mohammed zourob <fxfddfno@gmail.com>
2019-10-01 09:11:49 +0300

    users mangment

---
+++ src/services/users/users.hooks.js
@@ -0,0 +1,58 @@
+const { authenticate } = require("@feathersjs/authentication").hooks;
+
+const {
+  hashPassword,
+  protect
+} = require("@feathersjs/authentication-local").hooks;
+
+const adminAndRegisterAfterCreateUser = require("../../hooks/admin-and-register-after-create-user");
+
+const beforeUpdateUsers = require("../../hooks/before-update-users");
+
+const beforeDeleteUsers = require("../../hooks/before-delete-users");
+
+const permissions = require("../../hooks/permissions");
+
+const beforeGetUser = require('../../hooks/before-get-user');
+
+const afterFindUsers = require('../../hooks/after-find-users');
+
+module.exports = {
+  before: {
+    all: [permissions()],
+    find: [authenticate("jwt"), afterFindUsers()],
+    get: [authenticate("jwt"), beforeGetUser()],
+    create: [hashPassword("password"), adminAndRegisterAfterCreateUser()],
+    update: [
+      hashPassword("password"),
+      authenticate("jwt"),
+      beforeUpdateUsers()
+    ],
+    patch: [hashPassword("password"), authenticate("jwt")],
+    remove: [authenticate("jwt"), beforeDeleteUsers()]
+  },
+
+  after: {
+    all: [
+      // Make sure the password field is never sent to the client
+      // Always must be the last hook
+      protect("password")
+    ],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
